e80994ce81ff2028ccd3048b98d63c56d580a406
simiyu1 <mbchb1990@gmail.com>
2018-02-07 23:39:45 +0300

    My new Toast

--- app/src/main/java/andela/familyapp/familia/MainActivity.java
+++ app/src/main/java/andela/familyapp/familia/MainActivity.java
@@ -2,6 +2,7 @@ package andela.familyapp.familia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Miguna Miguna Mkenya", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Ouru", Toast.LENGTH_SHORT).show();
     }
 }
